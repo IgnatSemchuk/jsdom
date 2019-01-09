d54d2b175cb7d552f468e786430d4a1a1ccc091c
IgnatSemchuk <semchukignat@gmail.com>
2019-01-09 13:53:44 +0200

    enabled configure

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    console.time();
-
-    const catImages = [
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495636.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495625.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495622.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495619.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495616.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495613.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495592.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/495579.svg",
-        "https://storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/336916.svg"
-    ];
-
     function getSlider(inputSelector, slideList) {
         const amountSlides = slideList.length;
         let currentSlide = 0;
@@ -110,5 +94,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
-    const sliderInst = new getSlider('slider-container', catImages);
-});
+    new getSlider('slider-container', catImages);
